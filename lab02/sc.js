59a74fd6f8eea19ac9a65a3104de36e9557cdba1
Ibrahim aljabr <Ibrahim_i95@hotmail.com>
2020-12-28 16:16:57 +0200

    lap2

---
+++ lab02/sc.js
@@ -0,0 +1,109 @@
+'use strict'
+
+var winter=prompt("do you like winter (yes/y - no/n)")
+var winterlc=winter.toLowerCase();
+
+switch (winterlc) {
+    case "yes ":
+    case "y":
+        console.log(winterlc)
+        alert("I dont like it")
+        break;
+
+    case "no" :
+    case "n":
+        console.log(winterlc)
+    alert("Me to I dont like it")
+    break;
+
+    default:
+        alert("pick from the fowlloing (yes/y - no/n) ")
+        break;
+}
+
+var black=prompt("do you like black color (yes/y - no/n)")
+var blacklc=black.toLowerCase();
+
+switch (blacklc) {
+    case "yes ":
+    case "y":
+        console.log(blacklc)
+        alert("its my fav color")
+        break;
+
+    case "no" :
+    case "n":
+        console.log(blacklc)
+    alert("its ok")
+    break;
+
+    default:
+        alert("pick from the fowlloing (yes/y - no/n) ")
+        break;
+}
+
+var cats=prompt("do you like Cats (yes/y - no/n)")
+var catslc=winter.toLowerCase();
+
+switch (catslc) {
+    case "yes ":
+    case "y":
+        console.log(catslc)
+        alert("I like them to")
+        break;
+
+    case "no" :
+    case "n":
+        console.log(catslc)
+    alert("why!!")
+    break;
+
+    default:
+        alert("pick from the fowlloing (yes/y - no/n) ")
+        break;
+}
+
+var games=prompt("do you like videogames (yes/y - no/n)")
+var gameslc=winter.toLowerCase();
+
+switch (gameslc) {
+    case "yes ":
+    case "y":
+        console.log(gameslc)
+        alert("Me to I like videogames")
+        break;
+
+    case "no" :
+    case "n":
+        console.log(gameslc)
+    alert("ok")
+    break;
+
+    default:
+        alert("pick from the fowlloing (yes/y - no/n) ")
+        break;
+}
+
+var chocolate=prompt("do you like chocolate (yes/y - no/n)")
+var chocolatelc=chocolate.toLowerCase();
+
+switch (chocolatelc) {
+    case "yes ":
+    case "y":
+        console.log(chocolatelc)
+        alert("Me to I love it")
+        break;
+
+    case "no" :
+    case "n":
+        console.log(chocolatelc)
+    alert("NO, bad for you")
+    break;
+
+    default:
+        alert("pick from the fowlloing (yes/y - no/n) ")
+        break;
+}
+
+var name=prompt("What is your name ?");
+alert("Welcome "+name);
